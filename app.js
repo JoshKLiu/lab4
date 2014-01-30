051c542971c3ed4671099c703cd6882d8d6193e8
Joshua Liu <josh.k.liu@gmail.com>
2014-01-30 00:30:13 -0800

    node.js lab

--- app.js
+++ app.js
@@ -13,6 +13,8 @@ var index = require('./routes/index');
 // var user = require('./routes/user');
 
 var app = express();
+var index = require('./routes/index');
+var project = require('./routes/project');
 
 // all environments
 app.set('port', process.env.PORT || 3000);
@@ -36,9 +38,11 @@ if ('development' == app.get('env')) {
 
 // Add routes here
 app.get('/', index.view);
+app.get('/project/:name', project.viewProject);
 // Example route
 // app.get('/users', user.list);
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
 });
+
